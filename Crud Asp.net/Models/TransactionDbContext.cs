fec8af2e15fd001d87e5be9bd9bfbae6daffe183
Anthony <anthony.scottodaniello@ynov.com>
2020-07-15 15:01:47 +0200

    Update

---
+++ Crud Asp.net/Models/TransactionDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crud_Asp.net.Models
+{
+    
+    public class TransactionDbContext : DbContext
+    {
+        public TransactionDbContext(DbContextOptions<TransactionDbContext> options) : base(options)
+        { }
+
+        public DbSet<TransactionModel> Transactions { get; set; }
+    }
+
+}
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-Crud Asp.net
+# Asp.net Core MVC Crud using jQuery Ajax
+
+***url in a modal popup***
+
+***mvc crud operation with jQuery Ajax***
+
+***Loading spinner***
+
+***NoDirectAcess attribute***
